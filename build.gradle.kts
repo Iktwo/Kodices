f30d4063aadb26b39a6e8f84abbebbcb159158a6
Isaac SH <424675+Iktwo@users.noreply.github.com>
2025-08-22 08:44:16 -0600

    Add maven-publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
     alias(libs.plugins.androidLibrary).apply(false)
     alias(libs.plugins.benchmark).apply(false)
     alias(libs.plugins.jetbrainsCompose).apply(false)
-    alias(libs.plugins.compose.compiler) apply false
+    alias(libs.plugins.compose.compiler).apply(false)
     alias(libs.plugins.kotlin.android).apply(false)
     alias(libs.plugins.kotlinMultiplatform).apply(false)
     alias(libs.plugins.kover).apply(false)
@@ -18,9 +18,19 @@ plugins {
     alias(libs.plugins.org.jetbrains.kotlin.jvm).apply(false)
     alias(libs.plugins.serialization).apply(false)
     alias(libs.plugins.skie).apply(false)
+    `maven-publish`
+}
+
+publishing {
+    publications {
+        
+    }
 }
 
 allprojects {
+    group = "com.github.iktwo"
+    version = "0.1.0"
+
     apply(plugin = "org.jlleitschuh.gradle.ktlint")
 
     repositories {
