715f1f69b0b112a8a85bc8a9a514d4c5e122c04a
Isaac SH <424675+Iktwo@users.noreply.github.com>
2026-08-03 19:14:10 -0700

    Fix Gradle settings order and build memory limits

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,13 +1,3 @@
-rootProject.name = "KodicesProject"
-enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
-
-include(
-    ":Kodices",
-    ":Piktographs",
-    ":SampleApp",
-    ":KodexServer",
-)
-
 pluginManagement {
     repositories {
         gradlePluginPortal()
@@ -16,4 +6,14 @@ pluginManagement {
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
 }
-include(":SampleApp:sample_android")
+
+rootProject.name = "KodicesProject"
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+include(
+    ":Kodices",
+    ":Piktographs",
+    ":SampleApp",
+    ":SampleApp:sample_android",
+    ":KodexServer",
+)
